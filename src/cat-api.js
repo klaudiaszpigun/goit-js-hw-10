835f432ad4bdfe852d1d57db4ad53123043ecedf
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-02-19 12:37:58 +0100

    upd

---
+++ src/cat-api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const fetchBreeds = () => {
+  axios.defaults.headers.common['x-api-key'] =
+    'live_AJM1Q0DIcrSygQbnsg4pslo8fl1VJ4Zv7ypIl7hybaPRmZjhzGtPtwP2LjDEsSLm';
+
+  return axios.get(`https://api.thecatapi.com/v1/breeds`).then(res => res.data);
+};
+
+export const fetchCatByBreed = breedId => {
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .then(res => res.data);
+};
---
+++ src/index.js
@@ -0,0 +1,57 @@
+import Notiflix from 'notiflix';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+const breedSelectHtml = document.querySelector('.breed-select');
+const errorHtml = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader-wrapper');
+
+errorHtml.classList.add('hidden');
+loader.classList.remove('hidden');
+
+try {
+  fetchBreeds().then(data => {
+    renderBreeds(data);
+    loader.classList.add('hidden');
+  });
+} catch (error) {
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!'
+  );
+  loader.classList.add('hidden');
+}
+
+function renderBreeds(breeds) {
+  const markup = breeds
+    .map(({ name, id }) => `<option value="${id}">${name}</option>`)
+    .join('');
+  breedSelectHtml.innerHTML = markup;
+}
+
+breedSelectHtml.addEventListener('change', event => {
+  const breedId = event.target.value;
+  loader.classList.remove('hidden');
+  catInfo.innerHTML = '';
+  fetchCatByBreed(breedId)
+    .then(catData => {
+      renderCat(catData);
+      loader.classList.add('hidden');
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+      loader.classList.add('hidden');
+    });
+});
+
+function renderCat(catData) {
+  const { url } = catData[0];
+  const { description, temperament, name } = catData[0].breeds[0];
+  catInfo.innerHTML = `
+    <h2>${name}</h2>
+    <img src="${url}" alt="${name}" height="400"/>
+    <p>${description}</p>
+    <p><strong>Temperament:</strong> ${temperament}</p>
+  `;
+}
