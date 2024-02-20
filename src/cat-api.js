34b7d20844460d262b2386eef757d42c9339adb6
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-02-20 12:34:15 +0100

    upd

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,14 +1,29 @@
+// zaimportowanie biblioteki axios
 import axios from 'axios';
 
+// stworzenie callbacków któree będą importowane do innego pliku
+
+// callback zwracający tablicę obiektów z API
 export const fetchBreeds = () => {
+  // konieczność użycia klucza który dostaliśmy przy rejestracji
   axios.defaults.headers.common['x-api-key'] =
     'live_AJM1Q0DIcrSygQbnsg4pslo8fl1VJ4Zv7ypIl7hybaPRmZjhzGtPtwP2LjDEsSLm';
-
-  return axios.get(`https://api.thecatapi.com/v1/breeds`).then(res => res.data);
+  // zwrócenie tablicy obiektów z danymi
+  return (
+    axios
+      .get(`https://api.thecatapi.com/v1/breeds`)
+      // z ogromnej tablicy obiektów weź tylko dane z właściwośći name, tam są istotne dla nas informacje
+      .then(response => response.data)
+  );
 };
 
+// callback który ma w parametrze numer rasy
 export const fetchCatByBreed = breedId => {
-  return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(res => res.data);
+  return (
+    axios
+      // który będzie doklejany do linku
+      .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+      // a później będzie wyciągał informacje z właściwości data
+      .then(response => response.data)
+  );
 };
--- src/index.js
+++ src/index.js
@@ -6,33 +6,68 @@ const errorHtml = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader-wrapper');
 
+// ukrywamy error
 errorHtml.classList.add('hidden');
+// uwidaczniamy ładowanie
 loader.classList.remove('hidden');
 
+// sprawdzanie czy generowanie informacji o zdjęciu się powiedzie
+// jeśli tak to automatycznie ukrywane jest ładowanie na ekranie
 try {
-  fetchBreeds().then(data => {
-    renderBreeds(data);
-    loader.classList.add('hidden');
-  });
+  // wyciągnąć dane z backendu
+  fetchBreeds()
+    // jeśli się to uda to
+    .then(data => {
+      // wygeneruj informacje o zdjęciu
+      renderBreeds(data);
+      // i ukryj ładowanie
+      loader.classList.add('hidden');
+    });
 } catch (error) {
+  // jeśli wystąpił jakiś błąd to jest wyświetlany komunikat notify
   Notiflix.Notify.failure(
     'Oops! Something went wrong! Try reloading the page!'
   );
+  // i ukrywane jest ładowanie
   loader.classList.add('hidden');
 }
 
-function renderBreeds(breeds) {
+// funkcja renderująca opcje wyboru
+const renderBreeds = breeds => {
   const markup = breeds
+    // breeds to obiekt z wszystkimi rasami z kórego pobierane są dwa pola, które później wstawiane są do kodu
     .map(({ name, id }) => `<option value="${id}">${name}</option>`)
+    // łączone w string
     .join('');
+  // i dodawane do kodu HTML
   breedSelectHtml.innerHTML = markup;
-}
+};
 
+const renderCat = catData => {
+  const url = catData[0].url;
+  const description = catData[0].breeds[0].description;
+  const temperament = catData[0].breeds[0].temperament;
+  const name = catData[0].breeds[0].name;
+
+  catInfo.innerHTML = `
+    <h2>${name}</h2>
+    <img src="${url}" alt="${name}" height="400"/>
+    <p>${description}</p>
+    <p><strong>Temperament:</strong> ${temperament}</p>
+  `;
+};
+
+// przy naciśnięciu na wybranie rasy
 breedSelectHtml.addEventListener('change', event => {
+  // id rasy jest wartość elementu na który naciśniemy
   const breedId = event.target.value;
+  // wtedy ładowanie się pokazuje
   loader.classList.remove('hidden');
+  // w divie catInfo jest pusty string
   catInfo.innerHTML = '';
+  // wywołując funkcję która łaaduje informacje o zdjęciu, które zostało naciśnięte
   fetchCatByBreed(breedId)
+    // gdy wszystko się pobierze
     .then(catData => {
       renderCat(catData);
       loader.classList.add('hidden');
@@ -44,14 +79,3 @@ breedSelectHtml.addEventListener('change', event => {
       loader.classList.add('hidden');
     });
 });
-
-function renderCat(catData) {
-  const { url } = catData[0];
-  const { description, temperament, name } = catData[0].breeds[0];
-  catInfo.innerHTML = `
-    <h2>${name}</h2>
-    <img src="${url}" alt="${name}" height="400"/>
-    <p>${description}</p>
-    <p><strong>Temperament:</strong> ${temperament}</p>
-  `;
-}
