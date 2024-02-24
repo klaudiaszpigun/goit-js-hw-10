390e8dce63f2418629549033a3a15a5dd78ef5b9
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-02-24 12:52:45 +0100

    upd

--- src/cat-api.js
+++ src/cat-api.js
@@ -13,8 +13,45 @@ export const fetchBreeds = () => {
 };
 
 // eksportowaniie funnkcji która ma w parametrze ID rasy zwraca informacje dotyczące jego zdjęcia
-export const fetchCatByBreed = breedId => {
+export const fetchImageByBreed = breedId => {
   return axios
     .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     .then(response => response.data);
 };
+
+/*
+{
+  "id":"0XYvRd7oD",
+  "width":1204,"height":1445,
+  "url":"https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg",
+  "breeds":[{
+      "weight":{"imperial":"7  -  10","metric":"3 - 5"},
+      "id":"abys","name":"Abyssinian",
+      "temperament":"Active, Energetic, Independent, Intelligent, Gentle",
+      "origin":"Egypt",
+      "country_codes":"EG",
+      "country_code":"EG",
+      "life_span":"14 - 15",
+      "wikipedia_url":"https://en.wikipedia.org/wiki/Abyssinian_(cat)"
+}]
+}
+*/
+
+/*
+[{
+  "id":"ebv",
+  "url":"https://cdn2.thecatapi.com/images/ebv.jpg",
+  "width":176,"height":540,
+  "breeds":[{
+      "weight":{"imperial":"7  -  10","metric":"3 - 5"},
+      "id":"abys","name":"Abyssinian",
+      "temperament":"Active, Energetic, Independent, Intelligent, Gentle",
+      "origin":"Egypt",
+      "country_codes":"EG",
+      "country_code":"EG",
+      "life_span":"14 - 15",
+      "wikipedia_url":"https://en.wikipedia.org/wiki/Abyssinian_(cat)"
+  }]
+  "favourite":{}
+}]
+*/
--- src/index.js
+++ src/index.js
@@ -17,28 +17,42 @@ try {
   console.log(error);
 }
 
+// funkcja generująco opcje w rozwijanym menu
 const renderSelect = breeds => {
+  // iteruj po każdej rasie, znajduj id i name w każdym obiekcie i wklejaj te dane do kodu HTML
   const markup = breeds
     .map(({ name, id }) => `<option value="${id}">${name}</option>`)
     .join('');
   breedSelect.innerHTML = markup;
+  // i po wklejeniu danych ukryj ładowanie
   loader.classList.add('hidden');
 };
 
+// gdy będziee zdarzenie change na select
 breedSelect.addEventListener('change', event => {
+  // przypisz do zmiennej catId wartość atrybutu value => <option value="id">name</option>
   const catId = event.target.value;
+  // pokaż ładowanie
   loader.classList.remove('hidden');
+  // wyczyść całego diva z informacjami o kocie
   catInfo.innerHTML = '';
+  // wczytaj zdjęcie kota z Id podanym w zmiennej
   fetchImageByBreed(catId)
+    // gdy to sie uda to od razu generuj kod z danycmi o kocie
     .then(data => renderCat(data))
+    // jeśli nie to wyświetl alert
     .catch(error => {
       Notiflix.Notify.failure('Error checking cat:');
+      // i ukryj ładowanie
       loader.classList.add('hidden');
     });
 });
 
+// funkcja generująca dane o kocie mająca w parametrze dane o zdjęciu kota, każdy kot ma tablicę z jednym obiektem który jest zdjęciem
 const renderCat = catData => {
+  // dostanie się do pierwszego elementu i jego właściwości
   const url = catData[0].url;
+  // dostanie się do pierwszego elementu tablicy, który posiada również tablicę i dostanie się do jej pierwszego elementu
   const description = catData[0].breeds[0].description;
   const temperament = catData[0].breeds[0].temperament;
   const name = catData[0].breeds[0].name;
